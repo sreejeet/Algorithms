863972ae3987e28d1819594a8231e4b59bb52325
sreejeet <vsreejeet@gmail.com>
2019-11-19 12:11:41 +0530

    + queue.py

---
+++ python/queue/queue.py
@@ -0,0 +1,20 @@
+stack = []
+
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+
+print(stack)
+
+print(stack.pop(0))
+print(stack.pop(0))
+
+print(stack)
+
+""" Output
+[1, 2, 3, 4]
+1
+2
+[3, 4]
+"""
