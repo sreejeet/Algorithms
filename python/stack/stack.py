eb669c077439cfe4abcbd8b90b6e7e232b4a6d87
sreejeet <vsreejeet@gmail.com>
2019-11-19 12:05:46 +0530

    + stack.py

---
+++ python/stack/stack.py
@@ -0,0 +1,21 @@
+stack = []
+
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+
+print(stack)
+
+print(stack.pop())
+print(stack.pop())
+
+print(stack)
+
+""" Output
+[1, 2, 3, 4]
+4
+3
+2
+[1]
+"""
