c08fc598ee903805a9a512f61ef63a3e3cd909a0
V S Sreejeet <vsreejeet@gmail.com>
2020-09-01 11:51:13 +0530

    updated python/sorting/mergesort.py

--- python/sorting/mergesort.py
+++ python/sorting/mergesort.py
@@ -1,32 +1,34 @@
 import time
 
 def mergesorted(a):
-	if len(a) > 1:
-		m = len(a)//2
-		L = mergesorted(a[:m])
-		R = mergesorted(a[m:])
-		a = []
-		x = y = 0
-		while True:
-			Lx = L[x] if x < len(L) else None
-			Ry = R[y] if y < len(R) else None
-			if Lx == None and Ry == None:
-				break
-			if Lx == None:
-				a.append(Ry)
-				y+=1
-			elif Ry == None:
-				a.append(Lx)
-				x+=1
-			else:
-				if len(Lx) < len(Ry):
-					a.append(Lx)
-					x+=1
-				else:
-					a.append(Ry)
-					y+=1
-	return a
+	if len(a) <= 1:
+		return a
 
+	mid = len(a)//2
+	L = mergesorted(a[:mid])
+	R = mergesorted(a[mid:])
+	a = []
+	x = y = 0
+
+	while True:
+		Lx = L[x] if x < len(L) else None
+		Ry = R[y] if y < len(R) else None
+		if Lx == None and Ry == None:
+			break
+		if Lx == None:
+			a.extend(R[y:])
+			break
+		if Ry == None:
+			a.extend(L[x:])
+			break
+		if len(Lx) < len(Ry):
+			a.append(Lx)
+			x+=1
+		else:
+			a.append(Ry)
+			y+=1
+
+	return a
 
 if __name__ == '__main__':
 	a = [
